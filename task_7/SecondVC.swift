d7bf26209d16bbe5aa0439e4ab859465807ffaaa
Алексей Кононенко <alekseikononenko4@gmail.com>
2024-01-24 19:15:17 +0300

    Start feature

--- task_7/SecondVC.swift
+++ task_7/SecondVC.swift
@@ -1,11 +1,8 @@
-//
-//  SecondVC.swift
-//  task_7
-//
-//  Created by Алексей Кононенко on 23.01.24.
-//
-
 import Foundation
 import UIKit
 
-
+class SecondVC: UIViewController {
+    
+    var delegate: VCDelegate!
+    
+}
--- task_7/ViewController.swift
+++ task_7/ViewController.swift
@@ -1,18 +1,15 @@
-//
-//  ViewController.swift
-//  task_7
-//
-//  Created by Алексей Кононенко on 23.01.24.
-//
-
 import UIKit
+
+protocol VCDelegate {
+    func setName(name: String)
+}
 
 class ViewController: UIViewController {
     
     @IBOutlet private weak var nameLabel: UILabel!
     
-    @IBOutlet private weak var costumizeButton: UIButton!{
-        didSet{
+    @IBOutlet private weak var costumizeButton: UIButton! {
+        didSet {
             costumizeButton.layer.cornerRadius = costumizeButton.bounds.height / 2
             costumizeButton.layer.borderWidth = 3.0
             costumizeButton.layer.borderColor = (UIColor(red: 242.0/255.0, green: 116.0/255.0, blue: 119.0/255.0, alpha: 1.0)).cgColor
@@ -21,9 +18,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction private func pushDidTrapCustomize() {
+        let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
+        guard let secondVC = mainStoryBoard.instantiateViewController(withIdentifier: "SecondVC") as? SecondVC else { return }
+        secondVC.delegate = self
+        navigationController?.pushViewController(secondVC, animated: true)
+    }
+}
 
-
+extension ViewController: VCDelegate{
+    func setName(name: String) {
+        self.nameLabel.text = name
+    }
 }
 

5fb5a532d06fbc8b2f0663a664272159325b0db6
Алексей Кононенко <alekseikononenko4@gmail.com>
2024-01-24 19:35:42 +0300

    nameLabelFromSecondVC

--- task_7/ViewController.swift
+++ task_7/ViewController.swift
@@ -1,13 +1,13 @@
 import UIKit
-
+// интерфейс
 protocol VCDelegate {
     func setName(name: String)
 }
 
 class ViewController: UIViewController {
-    
+    // аутлет именного label
     @IBOutlet private weak var nameLabel: UILabel!
-    
+    // настройки визуала кнопки редактирования
     @IBOutlet private weak var costumizeButton: UIButton! {
         didSet {
             costumizeButton.layer.cornerRadius = costumizeButton.bounds.height / 2
@@ -15,11 +15,19 @@ class ViewController: UIViewController {
             costumizeButton.layer.borderColor = (UIColor(red: 242.0/255.0, green: 116.0/255.0, blue: 119.0/255.0, alpha: 1.0)).cgColor
         }
     }
+    //настройки визуала картинки аватара
+    @IBOutlet private weak var avatarImage: UIImageView! {
+        didSet {
+            avatarImage.layer.cornerRadius = avatarImage.bounds.height / 2
+            avatarImage.layer.borderWidth = 3.0
+            avatarImage.layer.borderColor = (UIColor(red: 242.0/255.0, green: 116.0/255.0, blue: 119.0/255.0, alpha: 1.0)).cgColor
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+    // действие на переход на второй экран
     @IBAction private func pushDidTrapCustomize() {
         let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
         guard let secondVC = mainStoryBoard.instantiateViewController(withIdentifier: "SecondVC") as? SecondVC else { return }
@@ -27,7 +35,7 @@ class ViewController: UIViewController {
         navigationController?.pushViewController(secondVC, animated: true)
     }
 }
-
+// передача информации из textField из второго экрана
 extension ViewController: VCDelegate{
     func setName(name: String) {
         self.nameLabel.text = name
